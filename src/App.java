c6fd4e9a6653c91119540f8911359d52839e60c2
AggiusVS <aggiusvellastefano@gmail.com>
2025-07-11 19:28:45 +0200

    incapsulamento

--- src/App.java
+++ src/App.java
@@ -3,9 +3,9 @@ public class App {
         
         Prodotto prodotto1 = new Prodotto("Tastiera", "Tastiera meccanica", 70.99, 22);
 
-        System.out.println("Codice: " + prodotto1.codice);
-        System.out.println("Nome: " + prodotto1.nome);
-        System.out.println("Descrizione: " + prodotto1.descrizione);
+        System.out.println("Codice: " + prodotto1.getCodice());
+        System.out.println("Nome: " + prodotto1.getNome());
+        System.out.println("Descrizione: " + prodotto1.getDescrizione());
         System.out.println("Prezzo Base: " + prodotto1.prezzoBase());
         System.out.println("Prezzo con iva: " + prodotto1.prezzoIva());
         System.out.println("Nome con codice: " + prodotto1.codiceNome());
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,11 +1,11 @@
 import java.util.Random;
 
 public class Prodotto {
-    public int codice;
-    public String nome;
-    public double prezzo;
-    public double iva;
-    public String descrizione;
+    private int codice;
+    private String nome;
+    private double prezzo;
+    private double iva;
+    private String descrizione;
 
     // costruttori
     public Prodotto(String nome, String descrizione, double prezzo, double iva) {
@@ -17,18 +17,57 @@ public class Prodotto {
         this.prezzo = prezzo;
 
     }
-    
 
     // metodi
-    public double prezzoBase(){
+    public double prezzoBase() {
         return prezzo;
     }
-    
-    public double prezzoIva(){
+
+    public double prezzoIva() {
         return prezzo + (prezzo * iva / 100);
     };
-    public String codiceNome(){
+
+    public String codiceNome() {
         return codice + "-" + nome;
     };
+
+    //getter 
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    //setter
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
     
+
+        
+
 }
